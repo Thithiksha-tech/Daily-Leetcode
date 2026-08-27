8b42701bf8877591acee9855d388a53a95272e99
THITHIKSHA A <thithikshathithiksha23@gmail.com>
2026-08-27 23:47:52 +0530

    Add solution for Two Sum

---
+++ LeetCode/Two-Sum/Solution.java
@@ -0,0 +1,13 @@
+1class Solution {
+2    public int[] twoSum(int[] nums, int target) {
+3        HashMap<Integer,Integer> map=new HashMap<>();
+4        for(int i=0;i<nums.length;i++){
+5            int complement=target-nums[i];
+6            if(map.containsKey(complement)){
+7                return new int[]{i,map.get(complement)};
+8            }
+9            map.put(nums[i],i);
+10        }
+11        return new int[]{};
+12    }
+13}
