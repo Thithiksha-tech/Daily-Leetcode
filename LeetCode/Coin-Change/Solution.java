beedcedb4e9fc0313c4eac16c8dc36fd29d3008f
THITHIKSHA A <thithikshathithiksha23@gmail.com>
2026-08-29 00:15:52 +0530

    Add solution for Coin Change

---
+++ LeetCode/Coin-Change/Solution.java
@@ -0,0 +1,19 @@
+1class Solution {
+2    public int coinChange(int[] coins, int amount) {
+3        int[] dp=new int[amount+1];
+4        Arrays.fill(dp,amount+1);
+5        dp[0]=0;
+6        for(int i=1;i<=amount;i++){
+7            for(int coin:coins){
+8                if(i>=coin){
+9                    dp[i]=Math.min(dp[i],dp[i-coin]+1);
+10                }
+11            }
+12        }
+13        if(dp[amount]==amount+1){
+14            return -1;
+15        }
+16        return dp[amount];
+17        
+18    }
+19}
