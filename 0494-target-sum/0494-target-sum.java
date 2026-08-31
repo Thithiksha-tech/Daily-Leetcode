a9020b2611e07df9764c2f8919b3143eb3fdf59a
THITHIKSHA A <thithikshathithiksha23@gmail.com>
2026-08-31 22:09:19 +0530

    Time: 6 ms (72.87%), Space: 47 MB (14%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,46 +1,36 @@
 class Solution {
-    static int count=0;
-    static int[][] dp;
-    public static int solve(int[] nums,int target,int ind,int sum,int tot){
-        if(ind==nums.length){
-            if(sum==target){
-                return 1;
-            }
-            return 0;
-            
-        }
-        int index=sum+tot;
-        if(dp[ind][index]!=-1){
-            return dp[ind][index];
-        }
-        int plusways=solve(nums,target,ind+1,sum+nums[ind],tot);
-        int minusways=solve(nums,target,ind+1,sum-nums[ind],tot);
-        dp[ind][index]=plusways+minusways;
-        return dp[ind][index];
-
-    }
     public int findTargetSumWays(int[] nums, int target) {
-        count=0;
-        // if(nums.length==1){
-        //     if(nums[0]==target||nums[0]==-target){
-        //         return 1;
-        //     }
-        //     else{
-        //         return 0;
-        //     }
-        // }
-        int tot=0;
-        for(int i=0;i<nums.length;i++){
-            tot+=nums[i];
-        }
         int n=nums.length;
-        dp=new int[n+1][2*tot+1];
-        for(int i=0;i<dp.length;i++){
-            Arrays.fill(dp[i],-1);
+        int tot=0;
+        for(int x:nums){
+            tot+=x;
+        } 
+         if (Math.abs(target) > tot) {
+            return 0;
         }
-        int ans=solve(nums,target,0,0,tot);
-    
-        return ans;
-        
+ 
+        int[][] dp=new int[n+1][2*tot+1];
+        dp[0][0+tot]=1;
+        for(int i=0;i<n;i++){
+            for(int sum=-tot;sum<=tot;sum++){
+                int ways=dp[i][sum+tot];
+                if(ways==0){
+                    continue;
+                }
+                dp[i+1][sum+nums[i]+tot]+=dp[i][sum+tot];
+                dp[i+1][sum-nums[i]+tot]+=dp[i][sum+tot];
+            }
+        }
+        return dp[n][tot+target];
     }
 }
+
+
+
+
+
+
+
+
+
+
