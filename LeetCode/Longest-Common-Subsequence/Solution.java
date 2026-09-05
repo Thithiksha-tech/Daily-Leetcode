d3b716ee45b0fdba6208e86dfd4621bebe0c7428
THITHIKSHA A <thithikshathithiksha23@gmail.com>
2026-09-05 22:12:44 +0530

    Add solution for Longest Common Subsequence

--- LeetCode/Longest-Common-Subsequence/Solution.java
+++ LeetCode/Longest-Common-Subsequence/Solution.java
@@ -1,19 +1,17 @@
 1class Solution {
-2    public int longestCommonSubsequence(String text1, String text2) {
-3        int[][] dp=new int[text1.length()+1][text2.length()+1];
-4        int n=text1.length();
-5        int m=text2.length();
-6        for(int i=1;i<=n;i++){
-7            for(int j=1;j<=m;j++){
-8                if(text1.charAt(i-1)!=text2.charAt(j-1)){
-9                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
-10                }
-11                else{
-12                    dp[i][j]=1+dp[i-1][j-1];
-13                }
-14            }
-15        }
-16        return dp[n][m];
-17        
-18    }
-19}
+2    public int longestCommonSubsequence(String t1, String t2) {
+3        int[][] dp=new int[t1.length()+1][t2.length()+1];
+4        for(int i=1;i<=t1.length();i++){
+5            for(int j=1;j<=t2.length();j++){
+6                if(t1.charAt(i-1)!=t2.charAt(j-1)){
+7                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+8                }
+9                else{
+10                    dp[i][j]=dp[i-1][j-1]+1;
+11                }
+12            }
+13        }
+14        return dp[t1.length()][t2.length()];
+15        
+16    }
+17}
