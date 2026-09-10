89dc90b6d9547af3a48ac6c8a436c09edad05de0
THITHIKSHA A <thithikshathithiksha23@gmail.com>
2026-09-10 23:08:45 +0530

    Add solution for All Paths From Source to Target

--- LeetCode/All-Paths-From-Source-to-Target/Solution.java
+++ LeetCode/All-Paths-From-Source-to-Target/Solution.java
@@ -1,28 +1,23 @@
 1class Solution {
-2    static List<List<Integer>> ans=new ArrayList<>();
-3    public static void solve(int[][] graph,int target,ArrayList<Integer> val,int ind){
-4    
-5        if(ind==target){
-6           
-7            ans.add(new ArrayList<>(val));
-8            return;
-9        }
-10        
-11        for(int i=0;i<graph[ind].length;i++){
-12     
-13            val.add(graph[ind][i]);
-14            solve(graph,target,val,graph[ind][i]);
-15            val.remove(val.size()-1);
-16        }
-17    }
-18
-19    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-20        ans.clear();
-21        int target=graph.length-1;
-22        ArrayList<Integer> val=new ArrayList<>();
-23        val.add(0);
-24        solve(graph,target,val,0);
-25        return ans;
-26        
-27    }
-28}
+2    public List<List<Integer>> allPathsSourceTarget(int[][] g) {
+3        List<List<Integer>> res=new ArrayList<>();
+4        Queue<List<Integer>> q=new LinkedList<>();
+5        List<Integer> st=new ArrayList();
+6        st.add(0);
+7        q.add(st);
+8        while(!q.isEmpty()){
+9            List<Integer> path=q.poll();
+10            int node=path.get(path.size()-1);
+11            if(node==g.length-1){
+12                res.add(path);
+13                continue;
+14            }
+15            for(int nei:g[node]){
+16                List<Integer> newpath=new ArrayList<>(path);
+17                newpath.add(nei);
+18                q.add(newpath);
+19            }
+20        }
+21        return res;
+22    }
+23}
