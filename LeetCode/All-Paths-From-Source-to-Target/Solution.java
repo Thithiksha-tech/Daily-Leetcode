f839bcfbc17f04a7dbe718923cbba41767d8614b
THITHIKSHA A <thithikshathithiksha23@gmail.com>
2026-09-11 11:35:06 +0530

    Add solution for All Paths From Source to Target

--- LeetCode/All-Paths-From-Source-to-Target/Solution.java
+++ LeetCode/All-Paths-From-Source-to-Target/Solution.java
@@ -1,23 +1,27 @@
 1class Solution {
 2    public List<List<Integer>> allPathsSourceTarget(int[][] g) {
-3        List<List<Integer>> res=new ArrayList<>();
-4        Queue<List<Integer>> q=new LinkedList<>();
-5        List<Integer> st=new ArrayList();
-6        st.add(0);
-7        q.add(st);
-8        while(!q.isEmpty()){
-9            List<Integer> path=q.poll();
-10            int node=path.get(path.size()-1);
-11            if(node==g.length-1){
-12                res.add(path);
-13                continue;
-14            }
-15            for(int nei:g[node]){
-16                List<Integer> newpath=new ArrayList<>(path);
-17                newpath.add(nei);
-18                q.add(newpath);
-19            }
-20        }
-21        return res;
-22    }
-23}
+3        Queue<List<Integer>> q=new LinkedList<>();
+4        List<List<Integer>> ans=new LinkedList<>();
+5        List<List<Integer>> result=new ArrayList<>();
+6        List<Integer> st=new ArrayList<>();
+7        st.add(0);
+8        q.add(st);
+9        while(!q.isEmpty()){
+10            
+11            List<Integer> path=q.poll();
+12            int node=path.get(path.size()-1);
+13            if(node==g.length-1){
+14                result.add(path);
+15                continue;
+16            }
+17            for(int nei:g[node]){
+18                List<Integer> newpath=new ArrayList<>(path);
+19                newpath.add(nei);
+20                q.add(newpath);
+21            }
+22
+23        }
+24        return result;
+25        
+26    }
+27}
